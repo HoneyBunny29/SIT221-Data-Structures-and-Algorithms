40275ab48a2857699bdc49eb61b48570f168c1b7
HoneyBunny🍯🐰 <corerakenisha@gmail.com>
2024-11-22 20:34:59 +0530

    Initial Commit

---
+++ Week 06/6.2D/6.2D/BoxOfCoins.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BoxOfCoins
+{
+    public class BoxOfCoins
+    {
+        public static int Solve(int[] boxes)
+        {
+            int sum = Sum(boxes);
+
+            int alexValue = AlexMaxCoins(boxes);
+            int cindyValue = sum - alexValue;
+
+            return alexValue - cindyValue;
+        }
+
+        private static int Sum(int[] boxes)
+        {
+            int sum = 0;
+            foreach (int item in boxes)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        // The recursion way, wayyyyyyy more expensive, 
+        // it will check the result in every single way
+        private static int AlexMaxCoins(int[] boxes, int start, int end)
+        {
+            if (start > end) return 0;
+
+            int a = boxes[start] + Math.Min(AlexMaxCoins(boxes, start + 2, end), AlexMaxCoins(boxes, start + 1, end - 1));
+            int b = boxes[end] + Math.Min(AlexMaxCoins(boxes, start + 1, end - 1), AlexMaxCoins(boxes, start, end - 2));
+
+            return Math.Max(a, b);
+        }
+
+        private static int AlexMaxCoins(int[] boxes)
+        {
+            // Time and complexity: O(n^2)
+            int n = boxes.Length;
+            int[,] table = new int[n, n];
+
+            int x, y, z;
+            for (int a = 0; a < n; ++a)
+            {
+                for (int i = 0, j = a; j < n; ++i, ++j)
+                {
+                    x = ((i + 2) <= j) ? table[i + 2, j] : 0;
+                    y = ((i + 1) <= (j - 1)) ? table[i + 1, j - 1] : 0;
+                    z = (i <= (j - 2)) ? table[i, j - 2] : 0;
+                    table[i, j] = Math.Max(boxes[i] + Math.Min(x, y), boxes[j] + Math.Min(y, z));
+                }
+            }
+            PrintTable(table);
+            return table[0, n - 1];
+        }
+        
+
+        private static void PrintTable(int[,] table)
+        {
+            int n = table.GetLength(0) - 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("{0,7}", table[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
---
+++ Week 08/8.2HD/8.2HD/8.2HD/CoinRepresentation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoinRepresentation
+{
+    /// <summary>
+    ///This class determines how many unique methods there are to use coins to represent a particular total.
+    /// For effective computing, it makes use of recursion along with memoization.
+    /// </summary>
+    public class CoinRepresentation
+    {
+        // A cache to hold previously calculated outcomes for subproblems
+        private static Dictionary<long, long> _cache = new Dictionary<long, long>();
+
+        /// <summary>
+        /// Determines how many ways there are to depict a given total.
+        /// </summary>
+        /// <param name="sum">The target sum to represent.</param>
+        /// <returns>how many different methods there are to express the amount.</returns>
+        public static long Solve(long sum)
+        {
+            // Base cases:
+            if (sum < 0) return 0; 
+            if (sum == 0) return 1;
+
+            // Verify if the outcome has previously been cached.
+            if (!_cache.ContainsKey(sum))
+            {
+                if (sum % 2 == 0)
+                {
+                    
+                    _cache.Add(sum, Solve(sum / 2) + Solve(sum / 2 - 1));
+                }
+                else
+                {
+                    // If sum is odd, reduce it and solve for (sum - 1) / 2
+                    _cache.Add(sum, Solve((sum - 1) / 2));
+                }
+            }
+
+            // Provide the result that was cached.
+            return _cache[sum];
+        }
+    }
+}
+
